a3c577d1273f05e14c48df37cbfa595d38105d3f
Pond <sanhapas.arun@gmail.com>
2026-01-20 20:36:01 +0700

    init project

---
+++ src/main/java/co/th/roadmap2026/Roadmap2026Application.java
@@ -0,0 +1,13 @@
+package co.th.roadmap2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Roadmap2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Roadmap2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/th/roadmap2026/Roadmap2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.th.roadmap2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Roadmap2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
